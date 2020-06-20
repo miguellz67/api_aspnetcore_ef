8a2caccbc4f69d86b4ba4b7a22b239b4b0829941
miguellz67 <luizmiguel67h@outlook.com>
2020-06-20 18:56:42 -0300

    uso do novo componente events

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      EventsComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
