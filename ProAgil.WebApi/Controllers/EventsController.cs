5b6d7fce52e75745bc9d9c8a5a5154ab5ef16fc0
miguellz67 <luizmiguel67h@outlook.com>
2020-06-20 16:10:00 -0300

    first commit

---
+++ ProAgil.WebApi/Controllers/EventsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebApi.Data;
+
+
+namespace ProAgil.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventsController(DataContext context)
+        {
+            _context = context;
+        }
+    
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var eves = await _context.Events.ToListAsync();
+                return Ok(eves);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "An error has ocurred on server");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEvent(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return this.StatusCode(StatusCodes.Status400BadRequest);
+                }
+                
+                var eve = await _context.Events.FindAsync(id);
+                
+                if(eve == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(eve);
+            }
+            catch (Exception)
+            {
+               return this.StatusCode(StatusCodes.Status500InternalServerError, "An error has ocurred on server");
+            }
+        }
+    }
+}
---
+++ ProAgil.WebApi/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProAgil.WebApi
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Theme { get; set; }
+        public int Peoples { get; set; }
+        public string Lot { get; set; }
+    }
+}
