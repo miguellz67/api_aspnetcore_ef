55e421afe4d4e9648cfc1c3de69f393a58adbc1c
miguellz67 <luizmiguel67h@outlook.com>
2020-06-25 17:15:48 -0300

    importação da nova dependência

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
@@ -15,7 +18,9 @@ import { NavComponent } from './nav/nav.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule,
+      Ng2SearchPipeModule
    ],
    providers: [],
    bootstrap: [
