21d9995b3aaac281616946ede66647f841c8c155
miguellz67 <luizmiguel67h@outlook.com>
2020-06-27 22:04:51 -0300

    correcao metodos

--- ProAgil.Repository/ProAgilRepository.cs
+++ ProAgil.Repository/ProAgilRepository.cs
@@ -1,6 +1,8 @@
-﻿using ProAgil.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,49 +10,108 @@ namespace ProAgil.Repository
 {
     class ProAgilRepository : IProAgilRepository
     {
+        private readonly DataContext _context;
+        public ProAgilRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        
+        // General
         public void Add<T>(T entity) where T : class
         {
-            throw new NotImplementedException();
+            _context.Add(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<Event> GetEventAsync(int EventId, bool includeSpeakers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Event[]> GetEventsAsync(bool includeSpeakers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Event[]> GetEventsAsyncByTheme(string theme, bool includeSpeakers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Speaker> GetSpeakerAsync(int SpeakerId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Speaker[]> GetSpeakersAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> SaveChangesAsync()
-        {
-            throw new NotImplementedException();
+            _context.Remove(entity);
         }
 
         public void Update<T>(T entity) where T : class
         {
-            throw new NotImplementedException();
+            _context.Update(entity);
         }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        // Events
+        public async Task<Event> GetEventAsync(int EventId, bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events.Include(c => c.Lots).Include(c => c.SocialNetWorks);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(pe => pe.EventSpeakers).ThenInclude(p => p.Speaker);
+            }
+
+            query = query.OrderByDescending(c => c.Date).Where(c => c.Id == EventId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Event[]> GetEventsAsync(bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events.Include(c => c.Lots).Include(c => c.SocialNetWorks);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(pe => pe.EventSpeakers).ThenInclude(p => p.Speaker);
+            }
+
+            query = query.OrderByDescending(c => c.Date);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Event[]> GetEventsAsyncByTheme(string theme, bool includeSpeakers = false)
+        {
+            IQueryable<Event> query = _context.Events.Include(c => c.Lots).Include(c => c.SocialNetWorks);
+
+            if (includeSpeakers)
+            {
+                query = query.Include(pe => pe.EventSpeakers).ThenInclude(p => p.Speaker);
+            }
+
+            query = query.OrderByDescending(c => c.Date).Where(c => c.Theme.ToLower().Contains(theme.ToLower()));
+
+            return await query.ToArrayAsync();
+        }
+
+        // Speakers
+        public async Task<Speaker> GetSpeakerAsync(int SpeakerId, bool includeEvents)
+        {
+            IQueryable<Speaker> query = _context.Speakers.Include(c => c.SocialNetWorks);
+
+            if (includeEvents)
+            {
+                query = query.Include(pe => pe.EventSpeakers).ThenInclude(p => p.Event);
+            }
+
+            query = query.OrderBy(c => c.Name).Where(c => c.Id == SpeakerId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Speaker[]> GetSpeakersAsync(bool includeEvents)
+        {
+            IQueryable<Speaker> query = _context.Speakers.Include(c => c.SocialNetWorks);
+
+            if (includeEvents)
+            {
+                query = query.Include(pe => pe.EventSpeakers).ThenInclude(p => p.Event);
+            }
+
+            query = query.OrderBy(c => c.Name);
+
+            return await query.ToArrayAsync();
+        }
+
+        
+
+        
     }
 }
