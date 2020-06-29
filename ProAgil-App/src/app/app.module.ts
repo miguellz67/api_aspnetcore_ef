b718c91fa45b275dd2c7d089a8a4a8c4f3033526
miguellz67 <luizmiguel67h@outlook.com>
2020-06-29 17:01:41 -0300

    import modulos  dropdown  tooltip  modal  e nova pipe

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -3,24 +3,34 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { NavComponent } from './nav/nav.component';
 
+import { DateTimeFormatPipe } from './_helps/DateTimeFormat.pipe';
 @NgModule({
    declarations: [
       AppComponent,
       EventsComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipe
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      Ng2SearchPipeModule
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot(),
+      Ng2SearchPipeModule,
+      BrowserAnimationsModule
    ],
    providers: [],
    bootstrap: [
